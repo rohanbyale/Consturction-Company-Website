e133132a53b52b83656ecc2a3be57a06165c1530
Rohan Byale <132317423+rohanbyale@users.noreply.github.com>
2026-05-27 23:08:04 +0530

    Update Footer.jsx

--- src/componets/Footer.jsx
+++ src/componets/Footer.jsx
@@ -16,7 +16,6 @@ const IndustrialFooter = () => {
       <div className="max-w-7xl mx-auto relative z-10 px-6 lg:px-16">
         <div className="grid grid-cols-1 lg:grid-cols-12 gap-12 border-b-4 border-black pb-16">
           
-          {/* Left Column: Branding & Socials */}
           <div className="lg:col-span-5 flex flex-col justify-between">
             <div>
               <div className="flex items-center gap-4 mb-8">
