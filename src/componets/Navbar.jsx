ce5323be5b8372a31cbf7c36173a705f24557d73
Rohan Byale <132317423+rohanbyale@users.noreply.github.com>
2026-06-18 18:54:11 +0530

    Update Navbar.jsx

--- src/componets/Navbar.jsx
+++ src/componets/Navbar.jsx
@@ -6,7 +6,7 @@ const WaabiNavbar = () => {
   const [isOpen, setIsOpen] = useState(false);
   const location = useLocation();
 
-  // Close menu automatically when route chang
+  // Close menu automatically when route chan
   useEffect(() => {
     setIsOpen(false);
   }, [location]);
