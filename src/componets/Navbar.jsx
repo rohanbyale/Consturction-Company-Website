1a9b5302d17d0a8f0f32f036c12378e5bf411396
Rohan Byale <132317423+rohanbyale@users.noreply.github.com>
2026-06-18 18:54:24 +0530

    Refactor Navbar component for improved functionality

--- src/componets/Navbar.jsx
+++ src/componets/Navbar.jsx
@@ -6,7 +6,7 @@ const WaabiNavbar = () => {
   const [isOpen, setIsOpen] = useState(false);
   const location = useLocation();
 
-  // Close menu automatically when route chan
+  // Close menu automatically when route cha
   useEffect(() => {
     setIsOpen(false);
   }, [location]);
